ccdcc158c0ea159f4b2ef798925b6be06d7816b0
Gonzodamus <Dick@TheDickWard.com>
2017-11-06 16:33:00 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+  get '/student/new', to: 'students#new'
   get '/student/:id', to: 'students#show', as: 'student'
+  post '/students', to: 'students#create', as: 'new_student'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create({first_name: 'Gary', last_name: 'Fitzgerald'})
+Student.create({first_name: 'Shane', last_name: 'Fitzshanea'})
+Student.create({first_name: 'Dana', last_name: 'Dandaniandan'})
+Student.create({first_name: 'Valambiak', last_name: 'Jones'})
+Student.create({first_name: 'Snorkbad', last_name: 'The Destroyer'})
+Student.create({first_name: 'Shelly', last_name: 'N/A'})
